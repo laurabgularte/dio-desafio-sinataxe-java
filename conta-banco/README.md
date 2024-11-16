b310b12ab89db9fc2ed3f1b56569827cacc5cb4f
Laura <laurabgularte@gmail.com>
2024-11-16 15:26:06 -0300

    add the final project and updated readme

--- conta-banco/README.md
+++ conta-banco/README.md
@@ -2,4 +2,6 @@
 
 Pondo em prática os fundamentos da linguagem e noções de regras de estrutura de sintaxe.
 
+A ideia do projeto é fazer com que o usuário digite as informações de sua conta bancária e, após isso, todos os seus dados apareçam na tela.
+
 
--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+     public static void main(String[] args) {
+       //iniciando scanner p/ solicitar dados
+                Scanner Scanner = new Scanner(System.in);
+
+                //input dos dados pelo usuario
+            
+                System.out.println("Digite seu nome: ");
+                String nome = Scanner.next();
+
+                System.out.println("Digite o número de sua agência: ");
+                String agencia = Scanner.next();
+
+                System.out.println("Digite o número de sua conta: ");
+                int numero = Scanner.nextInt();
+
+                System.out.println("Digite o saldo atual de sua conta: ");
+                double saldo = Scanner.nextDouble();
+
+                //apos o input dos dados, a saida: 
+
+                System.out.println("Olá, " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+                ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+                //finalizando o scanner 
+                Scanner.close();
+            }
+        }
+    
+
